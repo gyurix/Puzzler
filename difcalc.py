0c8a43f254ad83a03f6d0401b9062f13c03b888f
Gyurix <barathgy@gmail.com>
2019-03-11 15:18:00 +0100

    Excluded cursor position from difference calculation

--- difcalc.py
+++ difcalc.py
@@ -12,14 +12,16 @@ class IncorrectElementPosition(DifCalcStrategy):
         out = 0
         for x in range(s1.maxx()):
             for y in range(s1.maxy()):
-                out += s1.data[y][x] != s2.data[y][x]
+                if s1.data[y][x] != 'm':
+                    out += s1.data[y][x] != s2.data[y][x]
         return out
 
 
 class DistanceFromCorrectElementPosition(DifCalcStrategy):
-    def calc_dif(self, s1x, s1y, s1, s2, maxx, maxy):
+    @staticmethod
+    def calc_dif(s1x, s1y, s1, s2, maxx, maxy):
         d = s1[s1y][s1x]
-        if d == s2[s1y][s1x]:
+        if d == 'm' or d == s2[s1y][s1x]:
             return 0
         for x in range(maxx):
             for y in range(maxy):
@@ -36,4 +38,4 @@ class DistanceFromCorrectElementPosition(DifCalcStrategy):
         return out
 
 
-difcalcs = (IncorrectElementPosition(), DistanceFromCorrectElementPosition())
+dif_calculators = (IncorrectElementPosition(), DistanceFromCorrectElementPosition())
