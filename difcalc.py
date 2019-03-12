a62d9e904d21ebc434558c9b979efb8d44149ff1
Gyurix <barathgy@gmail.com>
2019-03-12 11:33:43 +0100

    Optimized difference calculator operations for loops (saved 10 seconds)

--- difcalc.py
+++ difcalc.py
@@ -4,6 +4,8 @@ import abc
 class DifCalcStrategy:
     def __init__(self, s2):
         self.s2 = s2
+        self.maxx = range(s2.maxx())
+        self.maxy = range(s2.maxy())
 
     @abc.abstractmethod
     def apply(self, s1):
@@ -13,8 +15,8 @@ class DifCalcStrategy:
 class DifCalcIncorrectEP(DifCalcStrategy):
     def apply(self, s1):
         out = 0
-        for x in range(s1.maxx()):
-            for y in range(s1.maxy()):
+        for x in self.maxx:
+            for y in self.maxy:
                 if s1.data[y][x] != 'm':
                     out += s1.data[y][x] != self.s2.data[y][x]
         return s1.dist + out
@@ -24,23 +26,20 @@ class DifCalcIncorrectEP(DifCalcStrategy):
 
 
 class DifCalcDistanceFromCorrectEP(DifCalcStrategy):
-    @staticmethod
-    def calc_dif(s1x, s1y, s1, s2, maxx, maxy):
+    def calc_dif(self, s1x, s1y, s1):
         d = s1.data[s1y][s1x]
-        if d == 'm' or d == s2.data[s1y][s1x]:
+        if d == 'm' or d == self.s2.data[s1y][s1x]:
             return 0
-        for x in range(maxx):
-            for y in range(maxy):
-                if s2.data[y][x] == d:
+        for x in self.maxx:
+            for y in self.maxy:
+                if self.s2.data[y][x] == d:
                     return abs(x - s1x) + abs(y - s1y)
 
     def apply(self, s1):
         out = 0
-        maxx = s1.maxx()
-        maxy = s1.maxy()
-        for x in range(maxx):
-            for y in range(maxy):
-                out += self.calc_dif(x, y, s1, self.s2, maxx, maxy)
+        for x in self.maxx:
+            for y in self.maxy:
+                out += self.calc_dif(x, y, s1)
         return s1.dist + out
 
     def __repr__(self):
